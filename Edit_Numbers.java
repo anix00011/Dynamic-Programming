e9b725547769929adf03244494270be17a774fe1
Anix00011 <31801904+anix00011@users.noreply.github.com>
2018-08-14 22:44:50 +0530

    Create Edit_Numbers.java

---
+++ Edit_Numbers.java
@@ -0,0 +1,21 @@
+package hp1;
+
+public class edit {
+    static void editnum(String str1,String str2,int m,int n){
+        int[][] mat=new int[m+1][n+1];
+        for(int i=0;i<=m;i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0) mat[i][j] = j;
+                else if (j == 0) mat[i][j] = i;
+                else if (str1.charAt(i - 1) == str2.charAt(j - 1)) mat[i][j] = mat[i - 1][j - 1];
+                else mat[i][j] = 1 + Math.min(mat[i - 1][j], Math.min(mat[i][j - 1], mat[i - 1][j - 1]));
+            }
+        }
+        System.out.println(mat[m][n]);
+    }
+    public static void main(String[] args) {
+        String str1 = "sunday";
+        String str2 = "saturday";
+        editnum(str1,str2,str1.length(),str2.length());
+    }
+}
